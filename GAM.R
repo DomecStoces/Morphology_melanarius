fb6c86ca76884bd58f6e57c0cbec17f5551f80e2
DomecStoces <dominik.stoces@mendelu.cz>
2026-04-02 13:38:48 +0200

    changes to calculation of RMA II, we did not had weighted means of sex. It is due to the fact, that some samples were not previsously determinated with sex, hence the weight.

--- GAM.R
+++ GAM.R
@@ -1,7 +1,7 @@
 library(mgcv)
 
 # Environmental drivers - visualize whether the male and female size curves diverge or converge as you move from rural (1) to urban (4) #
-gam_model <- gam(Elytra.length ~ Sex*Anthro_numeric + 
+gam_model <- gam(log(Elytra.length) ~ Sex*Anthro_numeric + 
                    s(Region, bs = "re"), 
                  data = df, weights = Predicted.sex, family=gaussian(link="identity"),
                  method = "REML")
@@ -14,7 +14,7 @@ concurvity(gam_model, full = TRUE)
 gratia::draw(gam_model)
 plot(gam_model, select = 2)
 
-gam_model2 <- gam(Elytra.length ~ Sex * Anthro_numeric1 + Habitat.type +
+gam_model2 <- gam(log(Elytra.length) ~ Sex * Anthro_numeric1 + Habitat.type +
                     s(Region, bs = "re"), weights = Predicted.sex, family=gaussian(link="identity"), 
                   data = df, method = "REML")
 summary(gam_model2)
@@ -24,7 +24,7 @@ gratia::draw(gam_model2)
 
 # When accounting for allometric scaling of body size, everything would be larger #
 # Include body size as covariate
-gam_model3 <- gam(Pronotum.length ~ 
+gam_model3 <- gam(log(Pronotum.length) ~ 
                    Sex * Anthro_numeric +
                    log(Elytra.length) +
                    s(Region, bs = "re"), weights = Predicted.sex, family=gaussian(link="identity"),
@@ -35,9 +35,9 @@ concurvity(gam_model3, full = TRUE)
 gratia::draw(gam_model3)
 
 # Do males and females scale differently with size?
-gam_model4 <- gam(Pronotum.width ~ 
-                   Sex * Anthro_numeric +
-                   Sex * log(Elytra.length) +
+gam_model4 <- gam(log(Head.length) ~ 
+                   Sex * Anthro_numeric1 +
+                  log(Elytra.length)*Sex +
                    s(Region, bs = "re"), weights = Predicted.sex, family=gaussian(link="identity"),
                  data = df, method = "REML")
 summary(gam_model4)
@@ -45,9 +45,9 @@ gam.check(gam_model4)
 concurvity(gam_model4, full = TRUE)
 gratia::draw(gam_model4)
 # Does allometry change along urban gradient?
-gam_model5 <- gam(Eye.distance ~ 
-                   Sex * Anthro_numeric +
-                   log(Elytra.length) * Anthro_numeric +
+gam_model5 <- gam(log(Elytra.width) ~ 
+                   Sex * Anthro_numeric1 +
+                   log(Elytra.length) * Anthro_numeric1 +
                    s(Region, bs = "re"), weights = Predicted.sex, 
                    family=gaussian(link="identity"),
                  data = df, method = "REML")
@@ -102,14 +102,14 @@ library(ggeffects)
 library(ggplot2)
 
 # Predict using the new FACTOR variable
-predicted_shape <- ggpredict(gam_model7, terms = c("Anthro_numeric1", "Sex"))
+predicted_shape <- ggpredict(gam_model6, terms = c("Anthro_numeric1", "Sex"))
 df_filtered$plot_x <- as.numeric(factor(df_filtered$Anthro_numeric1, levels = c(1, 3, 4)))
 predicted_shape$plot_x <- as.numeric(factor(predicted_shape$x, levels = c("1", "3", "4")))
 
 pd <- position_dodge(width = 0.2)
 d <- ggplot() +
   geom_jitter(data = df_filtered,
-              aes(x = plot_x, y = Shape_PC2, color = Sex), 
+              aes(x = plot_x, y = Size_PC1, color = Sex), 
               width = 0.15, height = 0, 
               alpha = 0.15, size = 1, na.rm = TRUE) + 
   geom_errorbar(data = predicted_shape, 
@@ -124,12 +124,12 @@ d <- ggplot() +
             linewidth = 1.2, position = pd) +
   scale_x_continuous(
     breaks = c(1, 2, 3), 
-    labels = c("Rural", "Suburban", "Urban")
+    labels = c("Natural", "Suburban", "Urban")
   ) +
   scale_color_manual(values = c("F" = "red", "M" = "blue")) + 
   labs(
     x = "Anthropogenic intensity",
-    y = "Body size (PC1 score)",
+    y = "Body shape (PC2 score)",
     color = "Sex"
   ) +
   theme_classic() +
--- PCA loadings.R
+++ PCA loadings.R
@@ -1,41 +1,49 @@
-# PCA on all traits #
-# Removing the Agrolandscape category
+# Removing the Agrolandscape category (assuming Anthro_numeric == 2 is Agrolandscape)
 df <- df %>%
   filter(Anthro_numeric != 2)
 
+# Select traits
 traits <- df[, c("Elytra.length", "Elytra.width", "Pronotum.length", 
                  "Pronotum.width", "Head.length", "Eye.distance")]
+
+# Filter for complete cases to run PCA
 complete_cases <- complete.cases(traits)
 traits_clean <- traits[complete_cases, ]
 
 # 2. RUN THE PCA
+# Log-transform, center, and scale as per methodology
 pca_result <- prcomp(log(traits_clean), center = TRUE, scale. = TRUE)
 
-# 3. View the results
+# 3. VIEW THE RESULTS
 summary(pca_result)
 print(pca_result$rotation)
 
-# 4. Initialize your new columns in the main dataframe with NA
+# 4. INSERT SCORES INTO MAIN DATAFRAME
 df$Size_PC1 <- NA
 df$Shape_PC2 <- NA
 
-# 5. Insert the PCA scores directly using complete_cases
-# (Multiply PC1 by -1 so positive values = larger body size)
+# Insert the PCA scores directly using complete_cases
+# (Multiply PC1 by -1 so positive values = larger body size, matching Table 1)
 df$Size_PC1[complete_cases] <- pca_result$x[, 1] * -1
 df$Shape_PC2[complete_cases] <- pca_result$x[, 2]
 
+# 5. OUTLIER FILTERING
 df_filtered <- df %>%
-  mutate(Size_PC1 = Size_PC1 * -1) %>% 
-  # Filter out extreme outliers (> 3.5 SDs)
-  filter(abs(as.numeric(scale(Size_PC1))) <= 3.5 & 
-           abs(as.numeric(scale(Shape_PC2))) <= 3.5)
+  # Filter out extreme outliers (> 3.5 SDs from the mean)
+  # using scale() safely by converting the matrix output to a vector
+  filter(
+    abs(as.vector(scale(Size_PC1))) <= 3.5 & 
+      abs(as.vector(scale(Shape_PC2))) <= 3.5
+  )
+
+# Check how many rows were dropped as outliers
+cat("Original rows:", nrow(df), "\nFiltered rows:", nrow(df_filtered), "\n")
 
 # PCA graph vizualization #
 library(ggplot2)
 library(dplyr)
 
 # 1. Bind the PCA scores back to your clean dataframe
-# Note: Multiplying PC1 by -1 to match your inverted interpretation in the methods
 df_plot <- cbind(df[complete_cases & df$Anthro_numeric != 2, ], 
                  PC1 = pca_result$x[,1] * -1, 
                  PC2 = pca_result$x[,2])
@@ -49,6 +57,3 @@ ggplot(df_plot, aes(x = PC1, y = PC2, color = as.factor(Anthro_numeric))) +
        x = "PC1: Body size (44.8%)",
        y = "PC2: Body shape (20.9%)",
        color = "Anthropogenic level")
-
-# 3. If you want to plot by Sex instead (assuming column is called 'Sex')
-# Just change the `color = Sex` argument in the aes() function.
